2bdc70a23e1476270c1f38120f2a9e95ae3fd25a
somej <aviesdfsdfsdnash.kumar@instarem.com>
2019-12-02 21:43:40 +0530

    reset to inital phase. removed three

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { RenderCanvas } from './containers/Canvas';
 import './App.css';
-import { Root, Ref } from './containers/Root';
 
 const App: React.FC = () => {
-  const ref = React.useRef<Ref>(null);
   return (
     <>
-    <Root ref={ref} />
-    {/* <RenderCanvas /> */}
-    {/* <EG /> */}
+    <div>sample</div>
     </>
   );
 }
